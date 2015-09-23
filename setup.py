987a9f89e808dad5a24fd42a85eab345adf4e6c2
Gabi Davar <grizzly.nyo@gmail.com>
2015-09-23 15:14:48 +0300

    setup.py refactor
    * drop pytest-cache req and require py.test 2.8
    * use setuptools-scm

--- setup.py
+++ setup.py
@@ -5,10 +5,18 @@ setup(
     description='pytest plugin to check source code with pyflakes',
     long_description=open("README.rst").read(),
     license="MIT license",
-    version='1.0.1',
+    use_scm_version={
+        'version_scheme': 'guess-next-dev',
+        'local_scheme': 'dirty-tag',
+    },
+    setup_requires=[
+        'setuptools>=18.0',
+        'setuptools-scm>1.5.4'
+    ],
     author='Florian Schulze, Holger Krekel and Ronny Pfannschmidt',
     author_email='florian.schulze@gmx.net',
     url='https://github.com/fschulze/pytest-flakes',
     py_modules=['pytest_flakes'],
     entry_points={'pytest11': ['flakes = pytest_flakes']},
-    install_requires=['pytest-cache', 'pytest>=2.3.dev14', 'pyflakes'])
+    install_requires=['pytest>=2.8', 'pyflakes']
+)
